9d21b9b3bc4e376d58ab686b755059193beee0cb
rochilandia <rochilandia@gmail.com>
2020-06-23 18:44:04 +0200

    PW2D2 Update

---
+++ ProjectsDS/Week2/day1/theory/mi_modulo.py
@@ -0,0 +1,29 @@
+def suma_2(a, b):
+    return a + b
+
+def resta_2(a, b):
+    return a - b
+
+
+Bonus: ¿Cómo meterías estos dos elementos en una lista llamada missing
+missing = []
+
+for titulo in biblioteca:
+    if titulo[1] == 0:
+        faltante = titulo[0]
+        print(faltante)
+        missing.append(faltante)
+
+c. Crea una variable que sea una lista llamada nuevo_libro en el que su primer elemento sea libro_1 y el segundo uds_1.
+
+d. Convierte a nuevo_libro a tupla. (muestra qué tipo es ahora esta variable)
+
+e. Añade nuevo_libro a la lista biblioteca
+
+
+Acaban de traer una unidad más de El mal de Corcira, añade una unidad más al segundo elemento del primer elemento de la lista biblioteca.
+t[1] = t[1]+1
+t
+
+biblioteca.append(nuevo_libro)
+biblioteca
